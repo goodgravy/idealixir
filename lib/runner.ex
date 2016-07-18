29d69ca23c75773a9ea9129cf7d2ca53205d18da
James Brady <james@jmsbrdy.com>
2016-07-18 17:03:07 +0100

    feat: add runner entry point with option parsing

--- lib/runner.ex
+++ lib/runner.ex
@@ -1,28 +1,47 @@
 defmodule Idealixir.Runner do
   def main(args) do
+    {command, extras} = args |> parse_args
+    process(command, extras)
   end
 
   def search(search_params \\ []) do
     {:ok, token} = Idealixir.Api.authenticate
 
-    {:ok, room_range} = params_to_range(search_params)
-    api_param = room_range_to_api_param(room_range)
+    {:ok, room_range} = params_to_rooms_range(search_params)
+    api_param = rooms_range_to_api_param(room_range)
     search_params = Keyword.put(search_params, :bedrooms, api_param)
 
     {:ok, properties} = Idealixir.Api.search(token, search_params)
-    properties |> Enum.filter(fn(property) ->
+    Enum.filter(properties, fn(property) ->
       Enum.member?(room_range, property.rooms)
     end)
   end
 
-  defp params_to_range(params) do
+  defp process(command, extras) do
+    case command do
+      "search" -> search(extras)
+      "echo"   -> echo(extras)
+      _        -> IO.puts "Sorry, only 'search' is supported at the moment (got #{command})"
+    end
+  end
+
+  defp echo(extras) do
+    IO.puts inspect(extras)
+  end
+
+  defp parse_args(args) do
+    {extras, [command], _} = OptionParser.parse(args)
+    {command, extras}
+  end
+
+  defp params_to_rooms_range(params) do
     case Keyword.get(params, :bedrooms) do
       nil -> {:ok, 1..100}
       bedrooms -> Idealixir.SearchParamParser.bedrooms(bedrooms)
     end
   end
 
-  defp room_range_to_api_param(bedrooms_range) do
+  defp rooms_range_to_api_param(bedrooms_range) do
     if Enum.min(bedrooms_range) > 4 do
       [4]
     else
